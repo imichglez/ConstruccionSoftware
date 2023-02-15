add528e66c6d210f79848c372d1dbe11a8105059
Ingrid González <106717319+imichglez@users.noreply.github.com>
2023-02-14 19:54:21 -0600

    Delete main.js

--- clase 1/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-function buttonColor() {  
-    document.getElementById("button").style.backgroundColor= '#cccccc';
-}
-
-function buttonColor2() {  
-    document.getElementById("button2").style.backgroundColor= '#cccccc';
-}
