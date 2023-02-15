b3c1d56d2a9ed2a8664cb3952224ada1b72f0f4e
Ingrid González <106717319+imichglez@users.noreply.github.com>
2023-02-14 20:12:59 -0600

    Add files via upload

---
+++ mod 3.1/assets/js/main.js
@@ -0,0 +1,7 @@
+function buttonColor() {  
+    document.getElementById("button").style.backgroundColor= '#cccccc';
+}
+
+function buttonColor2() {  
+    document.getElementById("button2").style.backgroundColor= '#cccccc';
+}
